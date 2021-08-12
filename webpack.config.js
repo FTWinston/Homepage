00ac07a5301cc0a3263146d43d702e04b9d6b623
FTWinston <ftwinston@gmail.com>
2021-08-12 21:18:26 +0100

    Trying html-critical-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
+const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
+const HtmlCriticalPlugin = require('html-critical-webpack-plugin');
+//const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin").default;
 
 const config = {
   plugins: [
@@ -32,7 +34,21 @@ const config = {
 module.exports = (env, argv) => {
   
   if (argv.mode === 'production') {
-    config.plugins.push(new HTMLInlineCSSWebpackPlugin());
+    config.plugins.push(
+      new HtmlCriticalPlugin({
+        base: path.join(path.resolve(__dirname), 'dist/'),
+        src: 'index.html',
+        dest: 'index.html',
+        inline: true,
+        minify: true,
+        extract: true,
+        width: 1024,
+        height: 768,
+        penthouse: {
+          blockJSRequests: false,
+        }
+      })
+    );
   }
 
   return config;
