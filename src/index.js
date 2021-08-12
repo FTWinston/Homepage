62ff58862e4bef852b21d9d9f602052e7605da0f
FTWinston <ftwinston@gmail.com>
2021-08-12 09:03:23 +0100

    Added entrace animation, and toggleable animation on second letter T

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.scss';
 
 const body = document.querySelector('body');
 const header = document.querySelector('.header');
+const text = document.querySelector('.header__text');
 const letters = document.getElementsByClassName('header__letter');
 const letterF = letters[0];
 const letterT1 = letters[1];
@@ -13,6 +14,8 @@ const letterT2 = letters[6];
 const letterO = letters[7];
 const letterN2 = letters[8];
 
+text.addEventListener('animationend', () => text.classList.remove('header__text--animateIn'));
+
 for (const letter of letters) {
     letter.addEventListener('click', e => e.target.classList.toggle('header__letter--active'));
 }
@@ -20,6 +23,7 @@ for (const letter of letters) {
 letterF.addEventListener('click', () => header.classList.toggle('header--font'));
 letterT1.addEventListener('click', () => header.classList.toggle('header--background'));
 letterI.addEventListener('click', () => body.classList.toggle('inverted'));
+letterT2.addEventListener('click', () => text.classList.toggle('header__text--animate'));
 
 // TODO: O face could be animated, rather than just text.
 
